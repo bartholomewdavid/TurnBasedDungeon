de234a523076bbe7b6743c51664f1e15cf6bbc85
David Bartholomew <david.bartholomew@rubikloud.com>
2016-03-28 13:03:14 -0400

    comments and refactoring

--- impact/lib/game/managers/inputmanager.js
+++ impact/lib/game/managers/inputmanager.js
@@ -1,138 +1,138 @@
-ig.module( 
-    'game.managers.inputmanager' 
+ig.module(
+  'game.managers.inputmanager'
 )
-.requires(
-    'impact.impact',
-	'game.managers.utils',
-	'plugins.underscore'
-)
-.defines(function(){
+  .requires(
+  'impact.impact',
+  'game.managers.utils',
+  'plugins.underscore'
+  )
+  .defines(function() {
 
-InputManager = ig.Class.extend({
-	game: undefined,
-	collisionData: undefined,
-	utils: undefined,
-	cursor: undefined,
-	commands: [],
-	
-	setGame: function( val ) {
-		this.game = val;
-		this.collisionData = this.game.collisionMap.data;
-		this.utils = new ig.Utils();
-		this.cursor = this.game.cursor;
-	},
+    InputManager = ig.Class.extend({
+      game: undefined,
+      collisionData: undefined,
+      utils: undefined,
+      cursor: undefined,
+      commands: [],
 
-	initInput: function() {
-		ig.input.initMouse();
-		ig.input.bind( ig.KEY.MOUSE1, 'CLICK' );
-	},
+      setGame: function(val) {
+        this.game = val;
+        this.collisionData = this.game.collisionMap.data;
+        this.utils = new ig.Utils();
+        this.cursor = this.game.cursor;
+      },
 
-	//Must be called manually
-    update: function() {
-		//When players turn and shit
-		if (true) {
-			var sur = undefined;
-			var PC = this.game.player.getCoordinates();
-			var game = this.game;
-			var cursor = this.cursor;
-			var collisionData = this.collisionData;
-			var utils = this.utils;
+      initInput: function() {
+        ig.input.initMouse();
+        ig.input.bind(ig.KEY.MOUSE1, 'CLICK');
+      },
 
-			//Add logic for game animations and shit
-			var gameState = game.getGameState();
-			var cursorActive = cursor.isActive();
-				
-			if (gameState == game.GAME_STATES.PLAYER_TURN_ANIMATING) {
-				return;
-			}				
-				
-			if (gameState == game.GAME_STATES.PLAYER_TURN_INPUT) {
-				if (ig.input.pressed( 'CLICK' )) {
-					if (cursorActive == true) {
-						var mx = ig.input.mouse.x + game.screen.x;
-						var my = ig.input.mouse.y + game.screen.y;
-						
-						for (var i = 0; i < cursor.currentActionEntities.length; i++) {
-							var pos = cursor.currentActionEntities[i].pos;
-							var size = cursor.currentActionEntities[i].size;
-							var cae = cursor.currentActionEntities[i];
+      //Must be called manually
+      update: function() {
+        //When players turn and shit
+        if (true) {
+          var sur = undefined;
+          var PC = this.game.player.getCoordinates();
+          var game = this.game;
+          var cursor = this.cursor;
+          var collisionData = this.collisionData;
+          var utils = this.utils;
 
-							if (mx >= pos.x &&
-								mx <= pos.x + size.x &&
-								my >= pos.y &&
-								my <= pos.y + size.y) {
-									cae.execute();
-							} 
-						}
-						
-						return;
-					}
-				}
-			}
+          //Add logic for game animations and shit
+          var gameState = game.getGameState();
+          var cursorActive = cursor.isActive();
 
-			if (ig.input.pressed( 'CLICK' )) {
-				if (cursorActive == false) {
-					var cursor = this.game.cursor;
-					cursor.setEntity(this.game.player);
-					cursor.clickPosition(ig.input.mouse);
-			        //Cursor Position
-			        var CP = this.utils.getCoordinates(cursor.pos);
-			        //Entity Position
-			        var EP = this.utils.getCoordinates(cursor.currentEntity.pos);
-					
-					// Ranges are created the way they are because outer range is exclusive
-			        if (CP.x == EP.x) {
-			            //Vertical
-						var inc = (EP.y > CP.y) ? 1 : -1;
-						var range = _.range(CP.y, EP.y, inc);
-						var valid = false;
-						if (valid) {
-			            	cursor.currentActions.push("Vertical");
-						}
-			        } else if (CP.y == EP.y) {
-			            //Horizontal
-						var inc = (EP.x > CP.x) ? 1 : -1;
-						var range = _.range(CP.x, EP.x, inc);
-						var valid = true;
-						_.each(range, function(val) {
-							if (utils.isWalkable({x: val, y: CP.y}, collisionData) == false) {
-								valid = false;
-								return;
-							}
-						})
-						if (valid) {
-							cursor.currentActions.push("Horizontal");
-						}
-			        } else if (Math.abs(CP.y - EP.y) == Math.abs(CP.x - EP.x)) {
-			            //Diagonal
-						var incX = (EP.x > CP.x) ? 1 : -1;
-						var incY = (EP.y > CP.y) ? 1 : -1;
-						var rangeX = _.range(CP.x, EP.x, incX);
-						var rangeY = _.range(CP.y, EP.y, incY);
-						var valid = true;
-						for (var i = 0; i < rangeX.length; i++) {
-							if (utils.isWalkable({x: rangeX[i], y: rangeY[i]}, collisionData) == false) {
-								valid = false;
-								return;
-							}
-						}
-						if (valid) {
-			            	cursor.currentActions.push("Diagonal");
-						}
-			        } else {
-			            //Strange Movement
-			        }
-			        
-			        cursor.currentActions.push("Jump");
-			        cursor.currentActions.push("Interact");
-			        cursor.currentActions.push("Examine");
-			        cursor.currentActions.push("Cancel");
-					
-			        cursor.game.cursor.displayActions();
-				}
-			}
-		}
-    }
-});
+          if (gameState == game.GAME_STATES.PLAYER_TURN_ANIMATING) {
+            return;
+          }
 
-});
+          if (gameState == game.GAME_STATES.PLAYER_TURN_INPUT) {
+            if (ig.input.pressed('CLICK')) {
+              if (cursorActive == true) {
+                var mx = ig.input.mouse.x + game.screen.x;
+                var my = ig.input.mouse.y + game.screen.y;
+
+                for (var i = 0; i < cursor.currentActionEntities.length; i++) {
+                  var pos = cursor.currentActionEntities[i].pos;
+                  var size = cursor.currentActionEntities[i].size;
+                  var cae = cursor.currentActionEntities[i];
+
+                  if (mx >= pos.x &&
+                    mx <= pos.x + size.x &&
+                    my >= pos.y &&
+                    my <= pos.y + size.y) {
+                    cae.execute();
+                  }
+                }
+
+                return;
+              }
+            }
+          }
+
+          if (ig.input.pressed('CLICK')) {
+            if (cursorActive == false) {
+              var cursor = this.game.cursor;
+              cursor.setEntity(this.game.player);
+              cursor.clickPosition(ig.input.mouse);
+              //Cursor Position
+              var CP = this.utils.getCoordinates(cursor.pos);
+              //Entity Position
+              var EP = this.utils.getCoordinates(cursor.currentEntity.pos);
+
+              // Ranges are created the way they are because outer range is exclusive
+              if (CP.x == EP.x) {
+                //Vertical
+                var inc = (EP.y > CP.y) ? 1 : -1;
+                var range = _.range(CP.y, EP.y, inc);
+                var valid = false;
+                if (valid) {
+                  cursor.currentActions.push("Vertical");
+                }
+              } else if (CP.y == EP.y) {
+                //Horizontal
+                var inc = (EP.x > CP.x) ? 1 : -1;
+                var range = _.range(CP.x, EP.x, inc);
+                var valid = true;
+                _.each(range, function(val) {
+                  if (utils.isWalkable({ x: val, y: CP.y }, collisionData) == false) {
+                    valid = false;
+                    return;
+                  }
+                })
+                if (valid) {
+                  cursor.currentActions.push("Horizontal");
+                }
+              } else if (Math.abs(CP.y - EP.y) == Math.abs(CP.x - EP.x)) {
+                //Diagonal
+                var incX = (EP.x > CP.x) ? 1 : -1;
+                var incY = (EP.y > CP.y) ? 1 : -1;
+                var rangeX = _.range(CP.x, EP.x, incX);
+                var rangeY = _.range(CP.y, EP.y, incY);
+                var valid = true;
+                for (var i = 0; i < rangeX.length; i++) {
+                  if (utils.isWalkable({ x: rangeX[i], y: rangeY[i] }, collisionData) == false) {
+                    valid = false;
+                    return;
+                  }
+                }
+                if (valid) {
+                  cursor.currentActions.push("Diagonal");
+                }
+              } else {
+                //Strange Movement
+              }
+
+              // cursor.currentActions.push("Jump");
+              cursor.currentActions.push("Interact");
+              cursor.currentActions.push("Examine");
+              cursor.currentActions.push("Cancel");
+
+              cursor.game.cursor.displayActions();
+            }
+          }
+        }
+      }
+    });
+
+  });
--- impact/lib/game/managers/pathfindingutils.js
+++ impact/lib/game/managers/pathfindingutils.js
@@ -1,145 +1,157 @@
 ig.module(
-    'game.managers.pathfindingUtils' 
+  'game.managers.pathfindingUtils'
 )
-.requires(
-    'impact.impact',
-	'game.managers.utils',
-	'game.entity.pathfinding.node',
-	'plugins.underscore'
-)
-.defines(function(){
+  .requires(
+  'impact.impact',
+  'game.managers.utils',
+  'game.entity.pathfinding.node',
+  'plugins.underscore'
+  )
+  .defines(function() {
 
-ig.PathfindingUtils = ig.Class.extend({
-	utils: undefined,
-	nodes: [],
-	
-    staticInstantiate: function() {
-        if( ig.PathfindingUtils.instance == undefined ) {
-            return null
+    ig.PathfindingUtils = ig.Class.extend({
+      utils: undefined,
+      nodes: [],
+
+      staticInstantiate: function() {
+        if (ig.PathfindingUtils.instance == undefined) {
+          return null
         }
         else {
-            return ig.PathfindingUtils.instance
+          return ig.PathfindingUtils.instance
         }
-    },
-	
-	init: function() {
-		ig.PathfindingUtils.instance = this
-		this.utils = new ig.Utils()
-	},
+      },
 
-	generateNodes: function(collisionData) {
-		// Generate nodes completely if doing this over again
-		this.nodes.forEach( function(entity) {
-			ig.game.removeEntity(entity)
-		})
-		
-		this.generateWalkNodes(collisionData)
-		this.generateClimbNodes(collisionData)
-	},
-	
-	generateWalkNodes: function(collisionData) {
-		var checkedPositions = []
-		
-		for (var y = 0; y < collisionData.length; y++) {
-			for (var x = 0; x < collisionData[y].length; x++) {
-				// Only check tiles that havent been checked yet
-				if (_.findWhere(checkedPositions, {x: x, y: y}) === undefined) {	
-					checkedPositions = checkedPositions.concat(this.generateWalkNode({x: x, y: y}, collisionData))
-				}
-			}
-		}
-	},
-	
-	generateWalkNode: function(position, collisionData) {
-		var checkedNodes = [position]
-		var positionsForNode = []
-		var utils = this.utils
-		
-		// Find all connected walkable positions
-		if (utils.isWalkable(position, collisionData)) {
-			// Proceed only is initial position is walkable
-			// Add initial position to the list
-			positionsForNode.push(_.clone(position))
-			this.walkableIterator(_.clone(position), collisionData, -1, 0, checkedNodes, positionsForNode)
-			this.walkableIterator(_.clone(position), collisionData, 1, 0, checkedNodes, positionsForNode)
-		}
-		// If more then 1 position is found, make a walk node
-		if (positionsForNode.length > 1) {
-			this.nodes.push(ig.game.spawnEntity(EntityNode, 0, 0, {'type': 'horizontal'}).setCoords(positionsForNode))
-		}
+      init: function() {
+        ig.PathfindingUtils.instance = this
+        this.utils = new ig.Utils()
+      },
 
-		// Return list of checked positions
-		return checkedNodes
-	},
-	
-	generateClimbNodes: function(collisionData) {
-		var checkedPositions = []
-		
-		for (var y = 0; y < collisionData.length; y++) {
-			for (var x = 0; x < collisionData[y].length; x++) {
-				// Only check tiles that havent been checked yet
-				if (_.findWhere(checkedPositions, {x: x, y: y}) === undefined) {	
-					checkedPositions = checkedPositions.concat(this.generateClimbNode({x: x, y: y}, collisionData))
-				}
-			}
-		}
-		
-		checkedPositions = []
-		
-		for (var y = 0; y < collisionData.length; y++) {
-			for (var x = 0; x < collisionData[y].length; x++) {
-				// Only check tiles that havent been checked yet
-				if (_.findWhere(checkedPositions, {x: x, y: y}) === undefined) {	
-					checkedPositions = checkedPositions.concat(this.generateClimbNode({x: x, y: y}, collisionData, true))
-				}
-			}
-		}
-	},
-	
-	generateClimbNode: function(position, collisionData, left) {
-		var checkedNodes = [position]
-		var positionsForNode = []
-		var utils = this.utils
-		var nodeType = (left) ? 'climb1' : 'climb2'
-		left = left || false
-		
-		// Find all connected walkable positions
-		if (utils.isWalkable(position, collisionData)) {
-			// Proceed only is initial position is walkable
-			// Add initial position to the list
-			positionsForNode.push(_.clone(position))
-			if (left) {
-				this.walkableIterator(_.clone(position), collisionData, -1, -1, checkedNodes, positionsForNode)
-				this.walkableIterator(_.clone(position), collisionData, 1, 1, checkedNodes, positionsForNode)
-			} else {
-				this.walkableIterator(_.clone(position), collisionData, -1, 1, checkedNodes, positionsForNode)
-				this.walkableIterator(_.clone(position), collisionData, 1, -1, checkedNodes, positionsForNode)
-			}
-		}
-		// If more then 1 position is found, make a walk node
-		if (positionsForNode.length > 1) {
-			this.nodes.push(ig.game.spawnEntity(EntityNode, 0, 0, {'type': nodeType}).setCoords(positionsForNode))
-		}
+      generateNodes: function(collisionData) {
+        // Generate nodes completely if doing this over again
+        this.nodes.forEach(function(entity) {
+          ig.game.removeEntity(entity)
+        })
 
-		// Return list of checked positions
-		return checkedNodes
-	},
-	
-	walkableIterator: function(position, collisionData, xIter, yIter, checkedNodes, positionsForNode) {
-		while (true) {
-			// Check all positions to the left
-			position.x += xIter
-			position.y += yIter
-			if (position.x < 0 || position.x >= collisionData.length-1) break;
-			// Add all nodes to checked list
-			checkedNodes.push(_.clone(position))
-			if (this.utils.isWalkable(position, collisionData)) {
-				positionsForNode.push(_.clone(position))
-			} else {
-				break;
-			}
-		}
-	}
-})
+        this.generateWalkNodes(collisionData)
+        this.generateClimbNodes(collisionData)
+      },
 
-})
+      generateWalkNodes: function(collisionData) {
+        var checkedPositions = []
+
+        for (var y = 0; y < collisionData.length; y++) {
+          for (var x = 0; x < collisionData[y].length; x++) {
+            // Only check tiles that havent been checked yet
+            if (_.findWhere(checkedPositions, { x: x, y: y }) === undefined) {
+              checkedPositions = checkedPositions.concat(this.generateWalkNode({ x: x, y: y }, collisionData))
+            }
+          }
+        }
+      },
+
+      generateWalkNode: function(position, collisionData) {
+        var checkedNodes = [position]
+        var positionsForNode = []
+        var utils = this.utils
+
+        // Find all connected walkable positions
+        if (utils.isWalkable(position, collisionData)) {
+          // Proceed only is initial position is walkable
+          // Add initial position to the list
+          positionsForNode.push(_.clone(position))
+          this.walkableIterator(_.clone(position), collisionData, -1, 0, checkedNodes, positionsForNode)
+          this.walkableIterator(_.clone(position), collisionData, 1, 0, checkedNodes, positionsForNode)
+        }
+        // If more then 1 position is found, make a walk node
+        if (positionsForNode.length > 1) {
+          this.nodes.push(ig.game.spawnEntity(EntityNode, 0, 0, { 'type': 'horizontal' }).setCoords(positionsForNode))
+        }
+
+        // Return list of checked positions
+        return checkedNodes
+      },
+
+      generateClimbNodes: function(collisionData) {
+        var checkedPositions = []
+
+        for (var y = 0; y < collisionData.length; y++) {
+          for (var x = 0; x < collisionData[y].length; x++) {
+            // Only check tiles that havent been checked yet
+            if (_.findWhere(checkedPositions, { x: x, y: y }) === undefined) {
+              checkedPositions = checkedPositions.concat(this.generateClimbNode({ x: x, y: y }, collisionData))
+            }
+          }
+        }
+
+        checkedPositions = []
+
+        for (var y = 0; y < collisionData.length; y++) {
+          for (var x = 0; x < collisionData[y].length; x++) {
+            // Only check tiles that havent been checked yet
+            if (_.findWhere(checkedPositions, { x: x, y: y }) === undefined) {
+              checkedPositions = checkedPositions.concat(this.generateClimbNode({ x: x, y: y }, collisionData, true))
+            }
+          }
+        }
+      },
+
+      generateClimbNode: function(position, collisionData, left) {
+        var checkedNodes = [position]
+        var positionsForNode = []
+        var utils = this.utils
+        var nodeType = (left) ? 'climb1' : 'climb2'
+        left = left || false
+
+        // Find all connected walkable positions
+        if (utils.isWalkable(position, collisionData)) {
+          // Proceed only is initial position is walkable
+          // Add initial position to the list
+          positionsForNode.push(_.clone(position))
+          if (left) {
+            this.walkableIterator(_.clone(position), collisionData, -1, -1, checkedNodes, positionsForNode)
+            this.walkableIterator(_.clone(position), collisionData, 1, 1, checkedNodes, positionsForNode)
+          } else {
+            this.walkableIterator(_.clone(position), collisionData, -1, 1, checkedNodes, positionsForNode)
+            this.walkableIterator(_.clone(position), collisionData, 1, -1, checkedNodes, positionsForNode)
+          }
+        }
+        // If more then 1 position is found, make a walk node
+        if (positionsForNode.length > 1) {
+          this.nodes.push(ig.game.spawnEntity(EntityNode, 0, 0, { 'type': nodeType }).setCoords(positionsForNode))
+        }
+
+        // Return list of checked positions
+        return checkedNodes
+      },
+
+      walkableIterator: function(position, collisionData, xIter, yIter, checkedNodes, positionsForNode) {
+        while (true) {
+          // Check all positions to the left
+          position.x += xIter
+          position.y += yIter
+          if (position.x < 0 || position.x >= collisionData.length - 1) break;
+          // Add all nodes to checked list
+          checkedNodes.push(_.clone(position))
+          if (this.utils.isWalkable(position, collisionData)) {
+            positionsForNode.push(_.clone(position))
+          } else {
+            break;
+          }
+        }
+      },
+
+      isWalkablePath: function(startPos, endPos, collisionData) {
+        // Prereqs:
+        //  - Refactor current nodes to be a larger node structure. Node group?
+        //  - Refactor generateNode functions to return a list of nodes
+        //  - Make a way to connect nodes together
+        //  - Is there even a reason to have nodegroups besides visualization?
+
+        // Make a list of visited nodes, + cost to get there (Empty list)
+        // Being at the end position
+        //
+      },
+    })
+
+  })
